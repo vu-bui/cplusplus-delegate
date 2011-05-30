385497960348ebf723ec577ed1de9d5e548c3a14
vuluanbui@gmail.com <vuluanbui@gmail.com@4cf29f10-cde4-1ad9-53e7-fc90de36f3cc>
2011-05-30 07:40:13 +0000

    add test directory

---
+++ trunk/test/delegate/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../../delegate/Delegate2.h"
+
+struct Test {
+	static double sum(double a, double b) {
+		std::cout << "static function sum(" << a << ", " << b << ")\n";
+		return a + b;
+	}
+
+	double substract(double a, double b) {
+		std::cout << "member function substract(" << a << ", " << b << ")\n";
+		return a - b;
+	}
+
+	double multiply(double a, double b) const {
+		std::cout << "const member function multiply(" << a << ", " << b << ")\n";
+	}
+};
+
+double divide(double a, double b) {
+	std::cout << "global function divide(" << a << ", " << b << ")\n";
+	return a / b;
+}
+
+int main(void) {
+	delegate::Delegate2<double, double, double> del(Test::sum);
+	Test t;
+	del.add(t, &Test::substract);
+	del.add(t, &Test::multiply);
+	del.add(divide);
+
+	std::cout << "return value: " << del(2, 3) << '\n';
+
+	return 0;
+}
